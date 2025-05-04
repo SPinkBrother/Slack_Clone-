5576a9db108ebc30a92e3a481ea1b82ea39b0b5f
NguyenHongAnh <spinkbrotherofficial@gmail.com>
2025-05-04 17:22:52 +0700

    first commit

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,10 @@
+import { AuthSCreen } from "@/features/auth/components/auth-screen";
+
+const AuthPage= () =>{
+    return(
+
+        <AuthSCreen></AuthSCreen>
+    )
+}
+
+export default AuthPage;
